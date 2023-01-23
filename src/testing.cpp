2b0dad65799df3722431c45028d39ada1e20d094
zpotts-cnu <zachary.potts.19@cnu.edu>
2023-01-23 01:28:05 -0500

    Testing added for distance matrix validity

--- src/testing.cpp
+++ src/testing.cpp
@@ -14,9 +14,20 @@ using namespace std;
 int main() {
 	string file = "data/test.txt";
 	DistanceMatrix dm(file);
-	EuclideanPoint* list = dm.getListOfPoints();
-	for (int i = 0; i<100; i++) {
-		cout<<(list[i]).getPointID();
-		cout<<endl;
-	}
+
+	// DistanceMatrix Testing
+//	float** distance_matrix = dm.getDistanceMatrix();
+//	for (int i = 0; i<dm.getSetSize(); i++) {
+//		for (int j = 0; j<dm.getSetSize(); j++) {
+//			cout<<distance_matrix[i][j]<<"\t";
+//		}
+//		cout<<endl;
+//	}
+
+	// Points List Testing
+//	EuclideanPoint* list = dm.getListOfPoints();
+//	for (int i = 0; i<100; i++) {
+//		cout<<(list[i]).getPointID();
+//		cout<<endl;
+//	}
 }
