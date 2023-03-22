9a0e7d98f16081eb24322250921d5d6969fe46d2
zpotts-cnu <zachary.potts.19@cnu.edu>
2023-03-22 03:17:59 -0400

    Included constants.h for dependency

---
+++ includes/constants.h
@@ -0,0 +1,15 @@
+/*
+ * constants.h
+ *
+ *  Created on: Mar 22, 2023
+ *      Author: potts
+ */
+
+#ifndef INCLUDES_CONSTANTS_H_
+#define INCLUDES_CONSTANTS_H_
+
+const int UNDEFINED = -1;
+
+
+
+#endif /* INCLUDES_CONSTANTS_H_ */
