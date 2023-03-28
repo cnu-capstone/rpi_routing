b06b69e0952222f06f874d29a416646cbe16846d
zpotts-cnu <zachary.potts.19@cnu.edu>
2023-03-28 00:17:11 -0400

    Updated routing for refactor for new data structures

--- includes/routing.h
+++ includes/routing.h
@@ -12,36 +12,25 @@
 #include <vector>
 #include <queue>
 #include <algorithm>
-#include <limits.h>
-#include <float.h>
-#include "DistanceMatrix.h"
-
-//#define INF INT_MAX
-#define INF FLT_MAX
+#include "Grid.h"
 
 // A class to represent a graph node
 class Node {
 public:
-    int id, dist;
-    Node(int id, int dist);
+	Node(GridLocation location);
+	~Node();
+	Node* parent;
+	GridLocation loc;
+	int g; 	// g is distance from src
+	int h;  // h is distance from dest
 };
 
 // A comparison class to be used in priority queue
 class CompareNodes {
 public:
     bool operator() (const Node& node1, const Node& node2) {
-        return node1.dist > node2.dist;
+        return (node1.g + node1.h) > (node2.g + node2.h);
     }
 };
-
-//int* nearest_neighbor(DistanceMatrix &matrix);
-
-//int* dijkstra
-
-std::vector<float> dijkstra(float **graph, int N, int src);
-
-void printShortestPath(float **graph, int N, int src, int dest);
-
-
 
 #endif /* INCLUDES_ROUTING_H_ */
