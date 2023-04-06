2f5217ec05788a192fbabea6e13d748c1f06018e
zpotts-cnu <zachary.potts.19@cnu.edu>
2023-04-05 23:24:13 -0400

    Added left and right lookup for cardinal directions in unordered_maps

--- includes/motor_driver.h
+++ includes/motor_driver.h
@@ -12,6 +12,13 @@
 #include "routing.h"
 #include "constants.h"
 
+typedef std::vector<std::bitset<INSTRUCTION_SIZE>> instruction_block;
+
+enum CARDINAL_DIR {NORTH, EAST, SOUTH, WEST};
+
+std::unordered_map<CARDINAL_DIR, CARDINAL_DIR> left ({{NORTH, WEST}, {WEST, SOUTH}, {SOUTH, EAST}, {EAST, NORTH}});
+std::unordered_map<CARDINAL_DIR, CARDINAL_DIR> right ({{NORTH, EAST}, {EAST, SOUTH}, {SOUTH, WEST}, {WEST, NORTH}});
+
 // Moved to constants.h
 //#define STALL 0  // {0,0,0};
 //#define RLEFT 1  // {0,0,1};
@@ -26,5 +33,9 @@ struct instruction {
 	uint8_t distance;
 //	std::bitset<INSTRUCTION_SIZE> binary_string;
 };
+
+//instruction_block subroute_to_instructions(GridLocation src, GridLocation dest, GridLocation prev);
+
+instruction_block route_to_instructions(CARDINAL_DIR facing, std::vector<Node> route);
 
 #endif /* INCLUDES_MOTOR_DRIVER_H_ */
--- objects/motor_driver.cpp
+++ objects/motor_driver.cpp
@@ -5,6 +5,120 @@
  *      Author: potts
  */
 
+#include "../includes/motor_driver.h"
 
+//instruction_block subroute_to_instructions(GridLocation src, GridLocation dest, GridLocation prev) {
+//	instruction_block block;
+//	// Which direction are we facing?
+//	CARDINAL_DIR facing;
+//	// If pos delta x -> Facing EAST (pos x direction)
+//	// If pos delta y -> Facing NORTH (pos y direction)
+//	// If neg delta x -> Facing WEST (neg x direction)
+//	// If neg delta y -> Facing SOUTH (neg y direction)
+//	int delta_x_from_prev = src.x - prev.x;
+//	int delta_y_from_prev = src.y - prev.y;
+//
+//	if (delta_x_from_prev > 0) {
+//		facing = EAST;
+//	}
+//	else if (delta_x_from_prev < 0) {
+//		facing = WEST;
+//	}
+//	else if (delta_y_from_prev > 0) {
+//		facing = NORTH;
+//	}
+//	else if (delta_y_from_prev < 0) {
+//		facing = SOUTH;
+//	}
+//
+//	CARDINAL_DIR driving;
+//
+//	int delta_x_to_dest = dest.x - src.x;
+//	int delta_y_to_dest = dest.y - src.y;
+//
+//	if (delta_x_to_dest > 0) {
+//		driving = EAST;
+//	}
+//	else if (delta_x_to_dest < 0) {
+//		driving = WEST;
+//	}
+//	else if (delta_y_to_dest > 0) {
+//		driving = NORTH;
+//	}
+//	else if (delta_y_to_dest < 0) {
+//		driving = SOUTH;
+//	}
+//
+//	std::pair<CARDINAL_DIR, CARDINAL_DIR> transition (facing, driving);
+//
+//
+//	// move in y direction
+//	if (abs(dest.y - src.y) > 0) {
+//
+//	}
+//	// move in x direction
+//	else if (abs(dest.x - src.x) > 0) {
+//
+//	}
+//	return block;
+//}
 
+instruction_block route_to_instructions(CARDINAL_DIR facing, std::vector<Node> route) {
+	instruction_block block;
+
+	for (int i = 0; i < route.size(); i++) {
+		if (i > 0) {
+			//	// Which direction are we facing?
+			//	// If pos delta x -> Facing EAST (pos x direction)
+			//	// If pos delta y -> Facing NORTH (pos y direction)
+			//	// If neg delta x -> Facing WEST (neg x direction)
+			//	// If neg delta y -> Facing SOUTH (neg y direction)
+			int delta_x_from_prev = route[i].loc.x - route[i - 1].loc.x;
+			int delta_y_from_prev = route[i].loc.x - route[i - 1].loc.x;
+
+			if (delta_x_from_prev > 0) {
+				facing = EAST;
+			}
+			else if (delta_x_from_prev < 0) {
+				facing = WEST;
+			}
+			else if (delta_y_from_prev > 0) {
+				facing = NORTH;
+			}
+			else if (delta_y_from_prev < 0) {
+				facing = SOUTH;
+			}
+		}
+
+		CARDINAL_DIR driving;
+//		switch (facing) {
+//		case NORTH:
+//			break;
+//		case EAST:
+//			break;
+//		case SOUTH:
+//			break;
+//		case WEST:
+//			break;
+//		}
+		// Facing N/S or E/W
+//		if (((facing == NORTH || facing == SOUTH) && (driving == NORTH || driving == SOUTH)) ||
+//				((facing == EAST || facing == WEST) && (driving == EAST || driving == WEST))) {
+//			// Go straight
+//		}
+		// If driving is not on the left or right of facing: we can go straight.
+		if (((*left.find(facing)).second != driving) || ((*right.find(facing)).second != driving)) {
+
+		}
+		else {
+			// We need to turn
+			// Left turn
+			if () {
+
+			}
+			// Right turn
+		}
+
+	}
+}
 
